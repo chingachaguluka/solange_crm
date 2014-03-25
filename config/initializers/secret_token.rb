0c631dd767d6105e55a8003986b3c000b2c6bd82
Chinga Chaguluka <chinga.e.chaguluka@gmail.com>
2014-03-25 13:40:36 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SolangeCrm::Application.config.secret_key_base = 'b7591170efb9376eb3657218e95f515120a52dfcbcccb4a279ebcdc1fe9f36829b0ff460665e617d35d3d029a33228ea08ba0c0a2faa837f79239484856ad98e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SolangeCrm::Application.config.session_store :cookie_store, key: '_solange_crm_session'
